a6394aa70df51734fa50d3221e7d2e1495cb6da3
Aaron McLeod <sircoolguy99@gmail.com>
2011-09-18 19:17:14 -0400

    first test started

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,3 @@
-require '../xmlparse'
+$LOAD_PATH.unshift(File.expand_path('../..', __FILE__))
+require 'xmlparse.rb'
+include XmlParser
--- spec/xmlparse_spec.rb
+++ spec/xmlparse_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
-describe XmlParser
-
+describe XmlParser do
+  
+  describe "init" do
+    before(:each) do
+      stub!(:gets).and_return('')
+      stub!(:puts)
+    end
+    
+    it "should give a greeting message 'type xml file name'" do
+      XmlParser.stub!(:build_headers).with(nil)
+      should_receive(:puts).with('type xml file name')
+      XmlParser::init
+    end
+    
+    
+  end
 end
