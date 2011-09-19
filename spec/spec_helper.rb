324fd446762974e6a2cf1ecd275e532bcc82a161
Aaron McLeod <sircoolguy99@gmail.com>
2011-09-18 21:17:13 -0400

    couple tests added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,43 @@
 $LOAD_PATH.unshift(File.expand_path('../..', __FILE__))
 require 'xmlparse.rb'
 include XmlParser
+
+class Person
+  attr_accessor :name, :email, :phone, :address
+  
+  def initialize(args)
+    args.each do |k, v|
+      send("#{k}=".to_sym, v)
+    end
+  end
+  
+  def to_xml
+    "<Person><name>#{name}</name><email>#{email}</email><phone>#{phone}</phone><address>#{address}</address></Person>"
+  end
+  
+  def missing_column
+    "<Person><name>#{name}</name><email>#{email}</email><address>#{address}</address></Person>"
+  end
+end
+
+def xml_header
+  '<?xml version="1.0" encoding="utf-8" ?>'
+end
+
+def load_test_files
+  people = []
+  people << Person.new(name: 'A person', email: 'test@something.com', phone: '555-233-3333', address: '444 test')
+  people << Person.new(name: 'John Smith', email: 'jsmith@fake.com', phone: '555-249-8833', address: '123 fake st')
+  people << Person.new(name: 'Jane Doe', email: 'example@something.ca', phone: '232-855-3422', address: '8643 Avenue Lane')
+  people << Person.new(name: 'Willis', email: 'nonexistant@example.com', phone: '416-233-7688', address: 'a test address')
+  File.open('test_file.xml', 'w+') do |f|
+    f.write("#{xml_header}\n")
+    people.each do |p|
+      f.write("#{p.to_xml}\n")
+    end
+  end
+end
+
+def remove_test_files
+  File.delete('test_file.xml')
+end
--- spec/xmlparse_spec.rb
+++ spec/xmlparse_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 describe XmlParser do
-  
   describe "init" do
     before(:each) do
-      stub!(:gets).and_return('')
-      stub!(:puts)
+      load_test_files
+      XmlParser.stub!(:gets).and_return('test_file.xml')
+      XmlParser.stub!(:puts)
     end
     
     it "should give a greeting message 'type xml file name'" do
-      XmlParser.stub!(:build_headers).with(nil)
-      should_receive(:puts).with('type xml file name')
+      XmlParser.stub!(:build_headers)
+      XmlParser.should_receive(:puts).with('type xml file name')
       XmlParser::init
     end
     
+    it "should call build_headers" do
+      XmlParser.stub!(:build_headers)
+      XmlParser.should_receive(:build_headers)
+      XmlParser::init
+    end
     
+    after(:each) do
+      remove_test_files
+    end
   end
 end
